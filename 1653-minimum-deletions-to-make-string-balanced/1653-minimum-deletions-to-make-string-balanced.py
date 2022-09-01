7bda4fb7a268819672c3f1c009e0b27cb337fcea
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-09-01 05:50:47 -0400

    Time: 2854 ms (6.23%), Space: 20.1 MB (12.55%) - LeetHub

---
+++ 1653-minimum-deletions-to-make-string-balanced/1653-minimum-deletions-to-make-string-balanced.py
@@ -0,0 +1,25 @@
+class Solution:
+    def minimumDeletions(self, s: str) -> int:
+        """
+        b cannot precede a
+        
+        every idx:
+            num b before it
+            num a after it
+        => ans = min(min(num_b[i], num_a[i])) for i in range(len(s))
+        """
+        num_a = [0 for _ in s] # num_a[i]: # of a after i
+        num_b = [0 for _ in s] # num_b[i]: # of b before i
+        for i in range(1, len(s)):
+            num_b[i] = num_b[i-1]
+            if s[i-1] == "b":
+                num_b[i] += 1
+        for i in range(len(s)-2, -1, -1):
+            num_a[i] = num_a[i+1]
+            if s[i+1] == "a":
+                num_a[i] += 1
+        ans = 10**5+1
+        for i in range(len(s)):
+            ans = min(ans, num_b[i] + num_a[i])
+        return ans
+        
