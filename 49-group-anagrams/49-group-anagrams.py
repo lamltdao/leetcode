34b738aefa02f7e834ff44d139df0708ea9f007e
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-09-02 05:49:04 -0400

    Time: 262 ms (6.96%), Space: 19 MB (34.16%) - LeetHub

--- 49-group-anagrams/49-group-anagrams.py
+++ 49-group-anagrams/49-group-anagrams.py
@@ -1,12 +1,14 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
-        sorted_str = ["".join(sorted(w)) for w in strs]
         d = {}
-        for i in range(len(sorted_str)):
-            if sorted_str[i] not in d:
-                d[sorted_str[i]] = [strs[i]]
-            else:
-                d[sorted_str[i]].append(strs[i])
+        for s in strs:
+            alp = [0 for _ in range(26)]
+            for c in s:
+                alp[ord(c) - ord('a')] += 1
+            t = tuple(alp)
+            if t not in d:
+                d[t] = []
+            d[t].append(s)
         result = []
         for v in d.values():
             result.append(v)
