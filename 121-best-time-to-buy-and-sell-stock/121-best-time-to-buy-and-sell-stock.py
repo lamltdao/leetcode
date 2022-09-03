8481e67cfedb91f37942fc6efe2132b4b78f7acf
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-09-03 07:47:35 -0400

    Time: 1505 ms (59.71%), Space: 25.2 MB (7.00%) - LeetHub

--- 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.py
@@ -4,14 +4,25 @@ class Solution:
         c1: dp[i]: highest price on the right of i O(2n)
         c2: sliding window O(n) 
         """
+        # Style 1
+        # l = 0
+        # r = 1
+        # ans = 0
+        # while r < len(prices):
+        #     if prices[l] < prices[r]:
+        #         ans = max(ans, prices[r] - prices[l])
+        #     else:
+        #         l = r
+        #     r += 1
+        # return ans
+        
+        # Style 2
         l = 0
-        r = 1
         ans = 0
-        while r < len(prices):
+        for r in range(len(prices)):
             if prices[l] < prices[r]:
                 ans = max(ans, prices[r] - prices[l])
             else:
                 l = r
-            r += 1
         return ans
         
