dbc7fc250e38c0a43b458790178d4db5cec78280
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-09-03 09:16:23 -0400

    Time: 229 ms (33.08%), Space: 14.1 MB (31.65%) - LeetHub

--- 567-permutation-in-string/567-permutation-in-string.py
+++ 567-permutation-in-string/567-permutation-in-string.py
@@ -1,28 +1,18 @@
 class Solution:
     def checkInclusion(self, s1: str, s2: str) -> bool:
-        s1_freq = {}
+        window_freq = {}
         for c in s1:
-            if c not in s1_freq:
-                s1_freq[c] = 0
-            s1_freq[c] += 1
+            if c not in window_freq:
+                window_freq[c] = 0
+            window_freq[c] += 1
         l = 0
-        r = 0
-        temp_s2_freq = {}
-        for c in s1_freq.keys():
-            temp_s2_freq[c] = s1_freq[c]
-        while r < len(s2) and r < len(s1):
-            if s2[r] in temp_s2_freq:
-                temp_s2_freq[s2[r]] -= 1
-            r += 1
-            
-        while l < len(s2):
-            if not any(temp_s2_freq.values()):
+        for r in range(len(s2)):
+            while l < r+1-len(s1):
+                if s2[l] in window_freq:
+                    window_freq[s2[l]] += 1
+                l += 1
+            if s2[r] in window_freq:
+                window_freq[s2[r]] -= 1
+            if not any(window_freq.values()):
                 return True
-            if s2[l] in s1_freq:
-                temp_s2_freq[s2[l]] = 1 if s2[l] not in temp_s2_freq else temp_s2_freq[s2[l]] + 1
-            l += 1
-            while r < len(s2) and r < l+len(s1):
-                if s2[r] in temp_s2_freq:
-                    temp_s2_freq[s2[r]] -= 1
-                r += 1
         return False
