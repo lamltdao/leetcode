6bd35fe739809c2f7f4ff647cca2206bca1019cc
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-09-03 07:40:39 -0400

    Time: 199 ms (21.69%), Space: 14 MB (93.15%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.py
@@ -3,15 +3,17 @@ class Solution:
         temp_set = set()
         l = 0
         r = 0
-        while r < len(s) and s[r] not in temp_set:
-            temp_set.add(s[r])
-            r += 1
+        # increment r as long as the window [l,r) is still valid
+        def inc_r():
+            nonlocal r, temp_set, s
+            while r < len(s) and s[r] not in temp_set:
+                temp_set.add(s[r])
+                r += 1
+        inc_r()
         ans = r - l
         while r < len(s):
             temp_set.remove(s[l])
             l += 1
-            while r < len(s) and s[r] not in temp_set:
-                temp_set.add(s[r])
-                r += 1
+            inc_r()
             ans = max(ans, r-l)
         return ans
