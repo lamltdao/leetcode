bbce325c1340397d0dd112419d83e8270a518c71
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-09-09 18:35:22 -0400

    Time: 6887 ms (5.02%), Space: 65.8 MB (94.20%) - LeetHub

---
+++ 1996-the-number-of-weak-characters-in-the-game/1996-the-number-of-weak-characters-in-the-game.py
@@ -0,0 +1,35 @@
+import bisect
+class Solution:
+    def numberOfWeakCharacters(self, properties: List[List[int]]) -> int:
+        properties.sort()
+        #list[ [attk_val, [def vals]] ]
+        sorted_grouped_props = []
+        i = 0
+        while i < len(properties):
+            j = i
+            group = [properties[i][0], []]
+            while j < len(properties) and properties[j][0] == properties[i][0]:
+                group[1].append(properties[j][1])
+                j += 1
+            sorted_grouped_props.append(group)
+            i = j
+        dp_r = [None for _ in sorted_grouped_props]
+        dp_r[-1] = len(sorted_grouped_props)-1
+        tmp_max_def = sorted_grouped_props[-1][1][-1]
+        """
+        dp[len-1] = len-1
+        dp[len-2] = len-1
+        dp[len-3] = max(len-1, len-2)
+        
+        """
+        for i in range(len(sorted_grouped_props)-2,-1,-1):
+            dp_r[i] = dp_r[i+1]
+            if sorted_grouped_props[i+1][1][-1] > tmp_max_def:
+                tmp_max_def = sorted_grouped_props[i+1][1][-1]
+                dp_r[i] = i+1
+        ans = 0
+        for i in range(len(dp_r)):
+            if dp_r[i] != i:
+                ans += bisect.bisect_left(sorted_grouped_props[i][1], sorted_grouped_props[dp_r[i]][1][-1])
+        return ans
+                    
