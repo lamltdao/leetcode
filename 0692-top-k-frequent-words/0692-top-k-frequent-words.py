9d38c13cc2c2d8d403754a672a981b11915bc930
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-10-13 13:48:57 -0400

    Time: 125 ms (29.68%), Space: 14.3 MB (6.54%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.py
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -1,5 +1,6 @@
 class Solution:
     def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        # Time: O(nlogn), Space: O(#unique words) = O(n)
         f = {}
         for w in words:
             if w not in f:
