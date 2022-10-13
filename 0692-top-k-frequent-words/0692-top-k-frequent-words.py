53547c9692bd193f9cc5201d8b0f768559a0d371
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-10-13 13:48:00 -0400

    Time: 137 ms (13.90%), Space: 14.6 MB (6.54%) - LeetHub

---
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.py
@@ -0,0 +1,22 @@
+class Solution:
+    def topKFrequent(self, words: List[str], k: int) -> List[str]:
+        f = {}
+        for w in words:
+            if w not in f:
+                f[w] = 0
+            f[w] += 1
+        class Item:
+            def __init__(self, word):
+                self.word = word
+            def __lt__(self, other):
+                if f[self.word] != f[other.word]:
+                    return f[self.word] > f[other.word]
+                return self.word < other.word
+        items = []
+        for w in f.keys():
+            items.append(Item(w))
+        items.sort()
+        ans = []
+        for i in range(k):
+            ans.append(items[i].word)
+        return ans
