8ace2d172a5a9347d399ca34ad558a0ff7e16776
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-10-13 20:26:02 -0400

    Time: 799 ms (9.12%), Space: 21.7 MB (81.43%) - LeetHub

---
+++ 1166-design-file-system/1166-design-file-system.py
@@ -0,0 +1,45 @@
+class Node:
+    def __init__(self):
+        self.children = {}
+        self.value = 0
+    def insert(self, list_path, value):
+        tmp = self
+        for i in range(len(list_path)):
+            d = list_path[i]
+            if d not in tmp.children:
+                if i != len(list_path)-1: # parent path does not exist
+                    return False
+                tmp.children[d] = Node()
+            tmp = tmp.children[d]
+        if tmp.value > 0: # already exist
+            return False
+        tmp.value = value
+        return True
+    def get_val(self, list_path):
+        tmp = self
+        for i in range(len(list_path)):
+            d = list_path[i]
+            if d not in tmp.children:
+                return -1 # path does not exist
+            tmp = tmp.children[d]
+        if tmp.value > 0:
+            return tmp.value
+        return -1
+class FileSystem:
+
+    def __init__(self):
+        self.trie = Node()
+
+    def createPath(self, path: str, value: int) -> bool:
+        list_path = path.split("/")[1:]
+        return self.trie.insert(list_path, value)
+
+    def get(self, path: str) -> int:
+        list_path = path.split("/")[1:]
+        return self.trie.get_val(list_path)
+
+
+# Your FileSystem object will be instantiated and called as such:
+# obj = FileSystem()
+# param_1 = obj.createPath(path,value)
+# param_2 = obj.get(path)
