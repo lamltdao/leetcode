d56292ac8c3ea37d817efb731e880a93a0c37cc2
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-10-28 21:17:13 -0400

    Time: 215 ms (93.96%), Space: 17.5 MB (37.92%) - LeetHub

---
+++ 0694-number-of-distinct-islands/0694-number-of-distinct-islands.py
@@ -0,0 +1,52 @@
+class Solution:
+    def numDistinctIslands(self, grid: List[List[int]]) -> int:
+        """
+        - find islands, store them
+        - d {island: cnt}, island 
+        
+        
+        for r 
+            for c
+                island = dfs
+                for i in each recorded island: O(mn)
+                    if not dup:
+                        add island to recorded island
+        """
+        islands = []
+        nrow = len(grid)
+        ncol = len(grid[0])
+        visited = [[False for _ in range(ncol)] for _ in range(nrow)]
+        def dfs(r,c,island,offset_r,offset_c):
+            nonlocal nrow, ncol
+            visited[r][c] = True
+            island.append((r-offset_r, c-offset_c))
+            dirs = [(0,1), (0,-1), (1,0), (-1,0)]
+            for d in dirs:
+                nr = r + d[0]
+                nc = c + d[1]
+                if 0 <= nr < nrow and 0 <= nc < ncol and not visited[nr][nc] and grid[nr][nc] == 1:
+                    dfs(nr,nc,island,offset_r,offset_c)
+        def is_duplicate(island_1, island_2):
+            if len(island_1) != len(island_2):
+                return False
+            l = len(island_1)
+            for i in range(l):
+                if island_1[i] != island_2[i]:
+                    return False
+            return True
+        for r in range(nrow):
+            for c in range(ncol):
+                if not visited[r][c] and grid[r][c] == 1:
+                    # store coordinates of island relative to the first visited cell
+                    island = []
+                    offset_r = r
+                    offset_c = c
+                    dfs(r,c,island, offset_r, offset_c)
+                    has_dup = False
+                    for exist_island in islands:
+                        if is_duplicate(island, exist_island):
+                            has_dup = True
+                            break
+                    if not has_dup:
+                        islands.append(island)
+        return len(islands)
