a68a1687821bbc34bd1d869bde5b63fb8a5d3579
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-10-29 13:18:29 -0400

    Time: 1172 ms (48.15%), Space: 15.6 MB (24.65%) - LeetHub

---
+++ 0875-koko-eating-bananas/0875-koko-eating-bananas.py
@@ -0,0 +1,34 @@
+import math
+class Solution:
+    def minEatingSpeed(self, piles: List[int], h: int) -> int:
+        """
+        k = max => len(piles) hour
+        
+        1 <= k<= max(piles)
+        
+        l < r:
+            m = (l+r) // 2
+        
+        n log n
+        
+        idea: try possible val of k in range [1, max(piles)]
+        if k is valid, try a smaller val
+        else if finish eating in less than h:
+            try smaller
+        else:
+            try bigger
+        """
+        l = 1
+        r = max(piles)
+        ans = None
+        def get_hour_finish(k):
+            return sum([math.ceil(p / k) for p in piles])
+        while l <= r:
+            m = (l+r) // 2
+            hour_finish = get_hour_finish(m)
+            if hour_finish <= h:
+                ans = m
+                r = m-1
+            else:
+                l = m+1
+        return ans
