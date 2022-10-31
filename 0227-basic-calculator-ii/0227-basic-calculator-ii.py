b8dbbe434874e22752852873d5cda00a04f49558
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-10-30 22:42:45 -0400

    Time: 94 ms (90.55%), Space: 15.7 MB (53.38%) - LeetHub

--- 0227-basic-calculator-ii/0227-basic-calculator-ii.py
+++ 0227-basic-calculator-ii/0227-basic-calculator-ii.py
@@ -2,12 +2,7 @@ class Solution:
     def calculate(self, s: str) -> int:
         numbers = set([str(i) for i in range(10)])
         operations = set(["+", "-", "*", "/"])
-        """
-        4+4*3*2+4
-        return 4 + eval
-        """
-        # Preprocess expression into arr
-        arr = []
+        stk = []
         i = 0
         sign = 1
         prev_op = None
@@ -20,13 +15,13 @@ class Solution:
                     i += 1
                 num = int(''.join(num_str))
                 if prev_op is None:
-                    arr.append(sign * num)
+                    stk.append(sign * num)
                 elif prev_op == "*":
-                    arr.append(arr.pop() * num)
+                    stk.append(stk.pop() * num)
                 else:
-                    n = arr.pop()
+                    n = stk.pop()
                     sign = 1 if n > 0 else -1
-                    arr.append(abs(n) // num * sign)
+                    stk.append(abs(n) // num * sign)
             elif c in operations:
                 if c == "+":
                     sign = 1
@@ -39,5 +34,5 @@ class Solution:
                 else:
                     prev_op = "/"  
             i += 1
-        return sum(arr)
+        return sum(stk)
         
