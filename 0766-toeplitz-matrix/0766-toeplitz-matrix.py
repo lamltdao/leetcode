f3154d29eab87b3f61923d8a67fe539c33c9d68b
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-10-30 22:19:01 -0400

    Time: 225 ms (14.05%), Space: 14 MB (36.74%) - LeetHub

---
+++ 0766-toeplitz-matrix/0766-toeplitz-matrix.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isToeplitzMatrix(self, matrix: List[List[int]]) -> bool:
+        """
+        for each cell in the first row
+            check each cell in the diagonal starting from that cell
+        for each cell in the first col excluding the top left
+            check same thing
+        """
+        nrow = len(matrix)
+        ncol = len(matrix[0])
+        for c in range(ncol):
+            val = matrix[0][c]
+            tmp_r = 1
+            tmp_c = c+1
+            while tmp_r < nrow and tmp_c < ncol:
+                if matrix[tmp_r][tmp_c] != val:
+                    return False
+                tmp_r += 1
+                tmp_c += 1
+        for r in range(1, nrow):
+            val = matrix[r][0]
+            tmp_r = r+1
+            tmp_c = 1
+            while tmp_r < nrow and tmp_c < ncol:
+                if matrix[tmp_r][tmp_c] != val:
+                    return False
+                tmp_r += 1
+                tmp_c += 1
+        return True
