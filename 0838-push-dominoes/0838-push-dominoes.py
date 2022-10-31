4d234b981275218a4ddd70707c1ea1032de1ee63
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-10-31 00:24:00 -0400

    Time: 1147 ms (22.15%), Space: 18.5 MB (40.11%) - LeetHub

--- 0838-push-dominoes/0838-push-dominoes.py
+++ 0838-push-dominoes/0838-push-dominoes.py
@@ -1,34 +1,5 @@
 class Solution:
     def pushDominoes(self, dominoes: str) -> str:
-        """
-        dp_l[i] = 1 means being pushed left
-        dp_r ... right
-        right:
-            loop from left to right
-                if dom[i] == R
-                    dp_r[i] = INF
-                    j = 1
-                    while arr[i+j] != "R":
-                        dp_r[i+j] = INF-j
-                        j += 1
-        same for left
-        
-        for each dominos:
-            if dp_l[i] > dp_r[i]:
-                => L
-            elif <:
-                => R
-            else:
-                => .
-                
-            ".L.R...LR..L.."
-        dp_l 
-        INF-1,INF,0,INF-4,INF-3,INF-2,INF-1,INF,INF-3,INF-2,INF-1,INF,0,0
-        dp_r 
-        0,0,0,INF,INF-1,INF-2,INF-3,INF-4,INF,INF-1,INF-2,INF-3, 0, 0
-        
-        [L,L,0,R,R,0,L,L,R,R,L,L,0,0]
-        """
         n = len(dominoes)
         forces = [0 for _ in range(n)]
         tmp_force = 0
