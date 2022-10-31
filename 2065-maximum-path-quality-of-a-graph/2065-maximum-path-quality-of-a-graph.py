64779393b67aa5d105e76314ab013be2098c89ff
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-10-31 12:08:59 -0400

    Time: 5023 ms (33.03%), Space: 16.8 MB (55.23%) - LeetHub

---
+++ 2065-maximum-path-quality-of-a-graph/2065-maximum-path-quality-of-a-graph.py
@@ -0,0 +1,28 @@
+class Solution:
+    def maximalPathQuality(self, values: List[int], edges: List[List[int]], maxTime: int) -> int:
+        ans = 0
+        n = len(values)
+        graph = [[] for _ in range(n)]
+        for e in edges:
+            u,v,w = e[0], e[1], e[2]
+            graph[u].append((v,w))
+            graph[v].append((u,w))
+        once = True
+        def bt(path, tmp_node, tmp_time, tmp_val):
+            nonlocal ans, maxTime,once
+            if once or tmp_node == 0:
+                ans = max(ans, tmp_val)
+                once = False
+            for next_node, w in graph[tmp_node]:
+                if tmp_time + w <= maxTime:
+                    if next_node not in path:
+                        path[next_node] = 0
+                        tmp_val += values[next_node]
+                    path[next_node] += 1
+                    bt(path, next_node, tmp_time+w, tmp_val)
+                    path[next_node] -= 1
+                    if path[next_node] == 0:
+                        del path[next_node]
+                        tmp_val -= values[next_node]
+        bt({0: 1}, 0, 0, values[0])
+        return ans
