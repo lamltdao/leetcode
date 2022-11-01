d1ff878d4e4a3c51b23c24f09f772708d298f022
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-11-01 13:04:17 -0400

    Time: 438 ms (57.30%), Space: 14.4 MB (55.41%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.py
@@ -0,0 +1,39 @@
+class Solution:
+    def findBall(self, grid: List[List[int]]) -> List[int]:
+        """
+        [
+        [1,1,1,1,1,1],
+        [-1,-1,-1,-1,-1,-1],
+        [1,1,1,1,1,1],
+        [-1,-1,-1,-1,-1,-1]
+        ]
+        
+        for each cell on the first row
+            try dropping it
+                if cell[r][c] ==1 and (not edge right or cell[r][c+1] == -1)
+                    c += 1
+                    r += 1
+                elif cell[r][c] == -1 and (not edge left or cell[r][c-1] == 1):
+                    c -= 1
+                    r += 1
+                else: # stuck    
+        """
+        nrow = len(grid)
+        ncol = len(grid[0])
+        ans = [-1 for _ in range(ncol)]
+        for c in range(ncol):
+            tmp_c = c
+            stuck = False
+            for r in range(nrow):
+                if grid[r][tmp_c] == 1 and not( (tmp_c+1 == ncol) or grid[r][tmp_c+1] == -1):
+                    tmp_c += 1
+                elif grid[r][tmp_c] == -1 and not( (tmp_c == 0) or grid[r][tmp_c-1] == 1):
+                    tmp_c -= 1
+                else:
+                    stuck = True
+                    break
+            if not stuck:
+                ans[c] = tmp_c
+        return ans
+            
+        
