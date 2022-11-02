f99ebc0e279a88db6580aa3a128cc2112848170d
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-11-02 15:16:55 -0400

    Time: 1437 ms (20.35%), Space: 14.4 MB (97.54%) - LeetHub

--- 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
+++ 1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/1368-minimum-cost-to-make-at-least-one-valid-path-in-a-grid.py
@@ -1,4 +1,3 @@
-# from heapq import heapify, heappush, heappop
 from collections import deque
 class Solution:
     def minCost(self, grid: List[List[int]]) -> int:
@@ -28,22 +27,4 @@ class Solution:
                         dist[nr][nc] = dist[r][c] + 1
                         q.append((nr,nc))
         return dist[nrow-1][ncol-1]
-        # pq = [(0, 0, 0)]
-        # heapify(pq)
-        # dirs = [(0,1,1), (0,-1,2), (1,0,3), (-1,0,4)]
-        # visited = [[False for _ in range(ncol)] for _ in range(nrow)]
-        # while len(pq) > 0:
-        #     cost, r,c = heappop(pq)
-        #     visited[r][c] = True
-        #     if r == nrow-1 and c == ncol-1:
-        #         return cost
-        #     for dr,dc,di in dirs:
-        #         nr = r + dr
-        #         nc = c + dc
-        #         if 0 <= nr < nrow and 0 <= nc < ncol and not visited[nr][nc]:
-        #             if di == grid[r][c]:
-        #                 heappush(pq,(cost, nr, nc))
-        #             else:
-        #                 heappush(pq,(cost+1, nr, nc))
-        # return -1
         
