2bcff8659a999e168e3a028abe159e1fc6ed9bb1
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-11-02 14:11:02 -0400

    Time: 821 ms (12.45%), Space: 17.6 MB (35.11%) - LeetHub

---
+++ 1439-find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows/1439-find-the-kth-smallest-sum-of-a-matrix-with-sorted-rows.py
@@ -0,0 +1,29 @@
+from heapq import heapify, heappush, heappop
+class Solution:
+    def kthSmallest(self, mat: List[List[int]], k: int) -> int:
+        """
+        at most 200 arr sums
+        1,3,11
+        2,4,6
+        """
+        nrow = len(mat)
+        ncol = len(mat[0])
+        tup = tuple([0 for _ in range(nrow)])
+        arr_sum = sum(mat[i][0] for i in range(nrow))
+        pq = [(arr_sum, tup)]
+        heapify(pq)
+        visited = set()
+        visited.add(tup)
+        while len(pq) > 0:
+            summ, ar = heappop(pq)
+            k -= 1
+            if k == 0:
+                return summ
+            for i in range(len(ar)):
+                if ar[i] + 1 < ncol:
+                    new_arr = tuple([ar[j] if j != i else ar[j]+1 for j in range(len(ar))])
+                    new_sum = summ - mat[i][ar[i]] + mat[i][new_arr[i]]
+                    if new_arr not in visited:
+                        visited.add(new_arr)
+                        heappush(pq, (new_sum, new_arr))
+        return -1
