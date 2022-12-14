f37326aa64ac3320891ed9c82c12e2b5df9d3093
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-12-14 12:01:30 -0500

    Time: 185 ms (86.39%), Space: 15.1 MB (80.71%) - LeetHub

--- 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
+++ 0845-longest-mountain-in-array/0845-longest-mountain-in-array.py
@@ -4,28 +4,45 @@ class Solution:
         less[i]: num increasing elements < i on the left
         greater[i]: num decreasing elements < i on the right
         """
-        less_l = [0 for _ in arr]
-        less_r = [0 for _ in arr]
-        
-        tmp_cnt = 0
-        for i in range(1, len(arr)):
-            if arr[i-1] < arr[i]:
-                tmp_cnt += 1
-            else:
-                tmp_cnt = 0
-            less_l[i] = tmp_cnt
-        tmp_cnt = 0
-        for i in range(len(arr)-2,-1,-1):
-            if arr[i+1] < arr[i]:
-                tmp_cnt += 1
-            else:
-                tmp_cnt = 0
-            less_r[i] = tmp_cnt
-        print(less_l, less_r)
+        if len(arr) < 3:
+            return 0
+        num_inc = num_dec = 0
+        is_inc = True
+        if arr[1] > arr[0]:
+            num_inc = 1
+        elif arr[1] < arr[0]:
+            num_dec = 1
+            is_inc = False
+        else:
+            is_inc = None
         ans = 0
-        for i in range(len(arr)):
-            if less_l[i] > 0 and less_r[i] > 0:
-                ans = max(ans, less_l[i] + less_r[i] + 1)
+        for i in range(2, len(arr)):
+            if is_inc == True:
+                if arr[i] > arr[i-1]:
+                    num_inc += 1
+                elif arr[i] < arr[i-1]:
+                    num_dec = 1
+                    is_inc = False
+                else:
+                    num_inc = num_dec = 0
+                    is_inc = None
+            elif is_inc == False:
+                if arr[i] < arr[i-1]:
+                    num_dec += 1
+                elif arr[i] > arr[i-1]:
+                    num_inc = 1
+                    is_inc = True
+                    num_dec = 0 # reset
+                else:
+                    num_inc = num_dec = 0
+                    is_inc = None
+            else:
+                if arr[i] > arr[i-1]:
+                    num_inc = 1
+                    is_inc = True
+                elif arr[i] < arr[i-1]:
+                    num_dec = 1
+                    is_inc = False
+            if num_inc > 0 and num_dec > 0:
+                ans = max(ans, num_inc + num_dec+1)
         return ans
-        
-            
