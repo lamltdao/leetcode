0e50a9cfcb0aef1059fe73d543770c3c011fc547
lamltdao <49036431+lamltdao@users.noreply.github.com>
2022-12-20 18:49:32 -0500

    Time: 44 ms (83.72%), Space: 14.5 MB (25.89%) - LeetHub

--- 0900-rle-iterator/0900-rle-iterator.py
+++ 0900-rle-iterator/0900-rle-iterator.py
@@ -10,15 +10,15 @@ class RLEIterator:
         self.cur_idx = 0 # index in self.arr
         self.cur_freq_cnt = 0 # freq counter in self.arr[cur_idx]
     def next(self, n: int) -> int:
-        tmp = n
+        num_exhaust = n
         while self.cur_idx < len(self.arr):
-            if self.cur_freq_cnt + tmp <= self.arr[self.cur_idx][0]:
-                self.cur_freq_cnt += tmp
+            if self.cur_freq_cnt + num_exhaust <= self.arr[self.cur_idx][0]:
+                self.cur_freq_cnt += num_exhaust
                 return self.arr[self.cur_idx][1]
             else:
-                tmp -= self.arr[self.cur_idx][0] - self.cur_freq_cnt
-                self.cur_freq_cnt = 0
-                self.cur_idx += 1
+                num_exhaust -= self.arr[self.cur_idx][0] - self.cur_freq_cnt # decrement num_exhaust
+                self.cur_freq_cnt = 0 # Reset to 0
+                self.cur_idx += 1 # Move to next el in self.arr
         return -1
 
 # Your RLEIterator object will be instantiated and called as such:
