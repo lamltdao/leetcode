fb7905793c7583dc61d6b416a639a3796cc97d24
lamltdao <49036431+lamltdao@users.noreply.github.com>
2023-04-12 01:01:58 -0400

    Time: 605 ms (29.38%), Space: 35.9 MB (26.79%) - LeetHub

--- 0518-coin-change-ii/0518-coin-change-ii.py
+++ 0518-coin-change-ii/0518-coin-change-ii.py
@@ -5,7 +5,7 @@ class Solution:
         dp[i][j]: number of comb to represent amount i using first j coins
         dp[0][j] = 1
         dp[i][0] = 1
-        dp[i][j] = sum {dp[i-coins[j]*k][j-1]} (use multiples of jth coin) + dp[i][j-1] (dont use jth coin)
+        dp[i][j] = dp[i-coins[j]][j]} (use jth coin) + dp[i][j-1] (dont use jth coin)
         """
         dp = [[0 for _ in range(len(coins)+1)] for _ in range(amount+1)]
         for j in range(len(coins)+1):
@@ -14,7 +14,5 @@ class Solution:
             for num_coin in range(1, len(coins)+1):
                 dp[am][num_coin] += dp[am][num_coin-1]
                 if am >= coins[num_coin-1]:
-                    # for mul in range(1, am//coins[num_coin-1]+1):
-                    #     dp[am][num_coin] += dp[am-mul*coins[num_coin-1]][num_coin-1]
                     dp[am][num_coin] += dp[am-coins[num_coin-1]][num_coin]
         return dp[amount][len(coins)]
