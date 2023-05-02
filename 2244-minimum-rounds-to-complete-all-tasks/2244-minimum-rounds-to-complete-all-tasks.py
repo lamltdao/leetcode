fe086e50db38cc0584f010864be1a003eb80e135
lamltdao <49036431+lamltdao@users.noreply.github.com>
2023-05-02 13:20:56 -0400

    Time: 968 ms (53.95%), Space: 31 MB (8.55%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -1,20 +1,10 @@
 class Solution:
     def minimumRounds(self, tasks: List[int]) -> int:
         d = {}
-        # {task_num: num occurences}
-        # if each task_num can be completed in rounds of 2 or 3, i.e != 1
         for t in tasks:
             if t not in d:
                 d[t] = 0
             d[t] += 1
-        # MAX = 10**6
-        # max_num_task = max(d.values())
-        # dp_min_round = [MAX for _ in range(max_num_task+1)]
-        # dp_min_round[0] = 0
-        # for i in range(2, max_num_task+1):
-        #     dp_min_round[i] = min(dp_min_round[i], dp_min_round[i-2]+1)
-        #     if i >= 3:
-        #         dp_min_round[i] = min(dp_min_round[i], dp_min_round[i-3]+1)
         def get_min_round(num):
             if num == 1:
                 return -1
