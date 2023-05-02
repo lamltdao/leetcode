d89bfa272f507cdbf9122676cf839b1f2b77ba59
lamltdao <49036431+lamltdao@users.noreply.github.com>
2023-05-02 13:20:40 -0400

    Time: 988 ms (36.31%), Space: 31.1 MB (6.16%) - LeetHub

--- 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
+++ 2244-minimum-rounds-to-complete-all-tasks/2244-minimum-rounds-to-complete-all-tasks.py
@@ -7,21 +7,26 @@ class Solution:
             if t not in d:
                 d[t] = 0
             d[t] += 1
-        MAX = 10**6
-        max_num_task = max(d.values())
-        dp_min_round = [MAX for _ in range(max_num_task+1)]
-        dp_min_round[0] = 0
-        for i in range(2, max_num_task+1):
-            dp_min_round[i] = min(dp_min_round[i], dp_min_round[i-2]+1)
-            if i >= 3:
-                dp_min_round[i] = min(dp_min_round[i], dp_min_round[i-3]+1)
+        # MAX = 10**6
+        # max_num_task = max(d.values())
+        # dp_min_round = [MAX for _ in range(max_num_task+1)]
+        # dp_min_round[0] = 0
+        # for i in range(2, max_num_task+1):
+        #     dp_min_round[i] = min(dp_min_round[i], dp_min_round[i-2]+1)
+        #     if i >= 3:
+        #         dp_min_round[i] = min(dp_min_round[i], dp_min_round[i-3]+1)
+        def get_min_round(num):
+            if num == 1:
+                return -1
+            if num % 3 == 1:
+                return 2 + (num-4) // 3
+            return num // 3 + (num - 3 * (num//3) ) // 2
         ans = 0
         for num_task in d.values():
-            if num_task == 1:
+            min_round = get_min_round(num_task)
+            if min_round == -1:
                 return -1
-            if dp_min_round[num_task] >= MAX:
-                return -1
-            ans += dp_min_round[num_task]
+            ans += min_round
         return ans
             
         
