f23b0ccc674f2709ef75fa1c3bb6b62bf811114c
lamltdao <49036431+lamltdao@users.noreply.github.com>
2023-05-10 23:17:00 -0400

    Time: 204 ms (48.82%), Space: 16.7 MB (33.86%) - LeetHub

--- 1035-uncrossed-lines/1035-uncrossed-lines.py
+++ 1035-uncrossed-lines/1035-uncrossed-lines.py
@@ -1,14 +1,5 @@
 class Solution:
     def maxUncrossedLines(self, nums1: List[int], nums2: List[int]) -> int:
-        """
-        if ij and ji => 1 line
-        
-        
-        2 5 1 2 5
-        
-        10 5 2 1 5 2
-        
-        """
         dp = [[0 for _ in range(len(nums2)+1)] for _ in range(len(nums1)+1)]
         for i in range(1, len(nums1)+1):
             for j in range(1, len(nums2)+1):
