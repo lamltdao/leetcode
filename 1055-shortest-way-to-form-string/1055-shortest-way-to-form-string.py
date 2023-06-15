4835b7398316dae10c70f86e373e8036626ff615
lamltdao <49036431+lamltdao@users.noreply.github.com>
2023-06-15 15:53:35 -0400

    Time: 83 ms (29.51%), Space: 16.3 MB (76.44%) - LeetHub

--- 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
+++ 1055-shortest-way-to-form-string/1055-shortest-way-to-form-string.py
@@ -9,17 +9,19 @@ class Solution:
         - 2 pointers, start at 0
         - try to match as many chars in source
         """
-        for c in target:
-            if c not in source:
-                return -1
         ans = 0
         target_ptr = 0
         while target_ptr < len(target):
             source_ptr = 0
-            while source_ptr < len(source) and target_ptr < len(target):
-                if source[source_ptr] == target[target_ptr]:
-                    target_ptr += 1
+            tmp = target_ptr
+            while source_ptr < len(source) and tmp < len(target):
+                if source[source_ptr] == target[tmp]:
+                    tmp += 1
                 source_ptr += 1
-            ans += 1
+            if tmp > target_ptr:
+                ans += 1
+                target_ptr = tmp
+            else:
+                return -1
         return ans
                 
