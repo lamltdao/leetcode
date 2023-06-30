b7c921b8ca031a0d83d3d55d62c06ea65bc419d6
lamltdao <49036431+lamltdao@users.noreply.github.com>
2023-06-30 14:29:22 -0400

    Time: 93 ms (47.48%), Space: 17 MB (99.54%) - LeetHub

--- 0422-valid-word-square/0422-valid-word-square.py
+++ 0422-valid-word-square/0422-valid-word-square.py
@@ -7,7 +7,6 @@ class Solution:
         """
         for w_idx, word in enumerate(words):
             for c_idx, char in enumerate(words[w_idx]):
-                # w_idx = 0, c_idx = 1
                 has_word_in_mirror_pos = c_idx < len(words) and w_idx < len(words[c_idx])
                 if not has_word_in_mirror_pos or words[w_idx][c_idx] != words[c_idx][w_idx]:
                     return False
