9bdc2cd24ee85de5eff8725c7d993843551975e1
lamltdao <49036431+lamltdao@users.noreply.github.com>
2023-07-02 22:03:59 -0400

    Time: 47 ms (72.86%), Space: 16.7 MB (28.16%) - LeetHub

--- 0859-buddy-strings/0859-buddy-strings.py
+++ 0859-buddy-strings/0859-buddy-strings.py
@@ -6,10 +6,13 @@ class Solution:
         for i in range(len(s)):
             if s[i] != goal[i]:
                 diff_idx.append(i)
+        # no difference, and there exists a char with >= 2 occurences, can just swap 2 occurences of that char
         if len(diff_idx) == 0 and len(set(s)) < len(s):
             return True
+        # differ in != 2 chars, can't swap just 2
         if len(diff_idx) != 2:
             return False
+        # check if swap-able
         idx1 = diff_idx[0]
         idx2 = diff_idx[1]
         if s[idx1] == goal[idx2] and s[idx2] == goal[idx1]:
