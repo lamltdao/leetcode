e1835ba3b110f193947917dbf28938c0c46f626f
lamltdao <49036431+lamltdao@users.noreply.github.com>
2023-07-04 02:30:09 -0400

    Time: 621 ms (20.00%), Space: 17.3 MB (90.00%) - LeetHub

---
+++ 0533-lonely-pixel-ii/0533-lonely-pixel-ii.py
@@ -0,0 +1,34 @@
+class Solution:
+    def findBlackPixel(self, picture: List[List[str]], target: int) -> int:
+        nrow = len(picture)
+        ncol = len(picture[0])
+        num_black_rows = [0 for _ in range(nrow)]
+        num_black_cols = [0 for _ in range(ncol)]
+        for r in range(nrow):
+            for c in range(ncol):
+                if picture[r][c] == 'B':
+                    num_black_rows[r] += 1
+                    num_black_cols[c] +=1
+        ans = 0
+        for c_idx, c in enumerate(num_black_cols):
+            if c != target:
+                continue
+            row_same = True
+            cur_row = None
+            for r_idx, r in enumerate(num_black_rows):
+                if picture[r_idx][c_idx] != 'B':
+                    continue
+                if r != target:
+                    row_same = False
+                    break
+                if cur_row is None:
+                    cur_row = picture[r_idx]
+                elif cur_row != picture[r_idx]:
+                    row_same = False
+                    break
+            print(c_idx, cur_row)
+            if row_same and cur_row is not None: # all rows the same + at least 1 row has black pixel
+                ans += target
+        return ans
+                    
+            
